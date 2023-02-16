e7fbe08e2f6534d434e1d14562cb9f56c8c2822b
Willian Cordeiro <107742701+darknessbarbie@users.noreply.github.com>
2023-02-16 13:48:13 -0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,13 @@
+package matematica_1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		int x, y;
+		x = 9;
+		y = 2 * x;
+		System.out.println(x);
+		System.out.println(y);
+	}
+
+}
